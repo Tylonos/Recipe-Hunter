89934c22b6f4cfb8f686c7475c402073a06562ab
Tylonos <nvikivan280@gmail.com>
2026-04-12 17:55:32 +0100

    Added the recipe details page along with the possibility to add pictures

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     cooking_time = models.IntegerField()
+    image = models.ImageField(upload_to='recipe_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
