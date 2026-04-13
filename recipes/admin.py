69a620e240813f76cc8906f379b5ad8a771d0d01
Silviu-Sri <serianu.andrei@gmail.com>
2026-04-13 17:48:15 +0100

    Bugfix 1

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient, Profile
+
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
     extra = 1
-from .models import Recipe
-from .models import Profile
+
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientInline]
 
+
 admin.site.register(Ingredient)
-admin.site.register(Recipe)
-admin.site.register( Profile)
+admin.site.register(RecipeIngredient)
+admin.site.register(Profile)
