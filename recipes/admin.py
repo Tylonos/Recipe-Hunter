9f35ecfc5db0f6b75ebec78c55f81f148aa3b07e
Radu Lupu <raduconstantin.lupu@student.griffith.ie>
2026-04-13 16:13:04 +0100

    Created login and register page

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 from .models import Recipe
+from .models import Profile
 
 admin.site.register(Recipe)
+admin.site.register( Profile)
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,11 @@
+
 from django.shortcuts import render, get_object_or_404
 from .models import Recipe
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import UserRegisterForm, ProfileForm
 
 
 def recipe_list(request):
@@ -10,3 +16,33 @@ def recipe_list(request):
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
     return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
+
+
+def register(request):
+    if request.method =='POST':
+        user_form = UserRegisterForm(request.POST)
+        profile_form = ProfileForm(request.POST, request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            messages.success(request,'Your account has been created. You can now log in.')
+            return redirect('recipes:login')
+    else:
+        user_form = UserRegisterForm()
+        profile_form =ProfileForm()
+    return render(request, 'recipes/register.html',{'user_form': user_form, 'profile_form': profile_form})
+
+
+class CustomLoginView(LoginView):
+    template_name ='recipes/login.html'
+
+
+class CustomLogoutView(LogoutView):
+    next_page ='recipes:login'
+
+
+@login_required
+def account(request):
+    return render(request, 'recipes/account.html')
